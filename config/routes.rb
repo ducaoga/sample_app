2f69f5d73863827e5d6d465e1506836a14701786
gaily <gailchan16@gmail.com>
2019-01-23 11:12:35 +0800

    end of chapter 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'user/new'
+
   root 'static_pages#home'
   get '/contact', to:'static_pages#contact'
 #  get '/home', to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
+  get  '/signup',  to: 'user#new'
 
 end
