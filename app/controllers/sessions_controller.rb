3acc0f1b92dded07d5090675327c8464b53af5f3
gaily <gailchan16@gmail.com>
2019-01-25 15:31:14 +0800

    end of chapter 9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,12 @@ class SessionsController < ApplicationController
 
   def create
   	#calling the record from the db
-  	user = User.find_by(email: params[:session][:email].downcase)
-  	if user && user.authenticate(params[:session][:password])
+  	?user = User.find_by(email: params[:session][:email].downcase)
+  	if ?user && ?user.authenticate(params[:session][:password])
   		#redirect to the paage 
-  		log_in user
-  		redirect_to user
+  		log_in ?user
+  		params[:session][:remember_me] == '1' ? remember ?user : forget(?user) 
+  		redirect_to ?user
   	else
   		#include error message
   		#if rendering same page like get ra di post. flash.now coz no request 
@@ -19,7 +20,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-  	log_out
+  	log_out if logged_in?
   	redirect_to root_url
   end
 end
